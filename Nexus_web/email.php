a114659859887a63342e4c0f2ffc2f9b5eca39b7
NuruzadeOmar <68065754+NuruzadeOmar@users.noreply.github.com>
2023-05-30 22:36:25 +0200

    php

--- Nexus_web/email.php
+++ Nexus_web/email.php
@@ -1,23 +1,31 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["Full Name"];
-  $company = $_POST["Company"];
-  $email = $_POST["Email"];
-  $message = $_POST["Message"];
-  
-  $to = "nuruzadeomar3@gmail.com"; // Replace with your email address
-  $subject = "NEXUS - New Contact Form Submission";
-  $body = "Name: $name\nCompany: $company\nEmail: $email\nMessage: $message";
-  
-  // Headers
-  $headers = "From: $email\r\n";
-  $headers .= "Reply-To: $email\r\n";
-  
-  // Send email
-  if (mail($to, $subject, $body, $headers)) {
-    echo "Email sent successfully.";
-  } else {
-    echo "Error sending email.";
-  }
+// Retrieve form data
+$fullName = $_POST['Full Name'];
+$companyName = $_POST['Company Name'];
+$email = $_POST['Email'];
+$message = $_POST['message'];
+
+// Create email content
+$subject = "NEXUS - New message from $fullName";
+$body = "Company: $companyName\n\n";
+$body .= "Email: $email\n\n";
+$body .= "Message: $message";
+
+// Set up email headers
+$headers = "From: $fullName <$email>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
+
+// Send the email
+$recipientEmail = "recipient@example.com"; // Replace with the recipient's email address
+$mailSent = mail($recipientEmail, $subject, $body, $headers);
+
+// Check if the email was sent successfully
+if ($mailSent) {
+  echo "Email sent successfully.";
+} else {
+  echo "Failed to send email.";
 }
 ?>
